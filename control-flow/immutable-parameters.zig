d5a173ade529ec226aa7d364ee297d15b8812f3c
thandoncube1 <ncubethando20@gmail.com>
2025-05-21 02:15:01 -0400

    Adding the immutability and making it mutable - params

--- control-flow/immutable-parameters.zig
+++ control-flow/immutable-parameters.zig
@@ -1,1 +1,13 @@
 const std = @import("std");
+
+fn add2(x: u32) u32 {
+    // Mutation doesn't work on constants in Zig, parameters
+    // are constants e.g. x = x + 2; immutable constant error
+
+    return x + 2;
+}
+
+pub fn main() !void {
+    const y = add2(4);
+    std.debug.print("{d}\n", .{y});
+}
