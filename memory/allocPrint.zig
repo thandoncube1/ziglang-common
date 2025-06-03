746d2cba3044fbdee9496684185b574e1f657f78
thandoncube1 <ncubethando20@gmail.com>
2025-06-03 15:59:44 -0400

    Working with memory allocation.

---
+++ memory/allocPrint.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const name = "Thando";
+    const output = try std.fmt.allocPrint(allocator, "Hello {s}!!!", .{name});
+
+    std.debug.print("{s}\n", .{output});
+}
