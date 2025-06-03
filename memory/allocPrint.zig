c07cd660d8b77656be56a26fddd8eb73ce7b7fa3
thandoncube1 <ncubethando20@gmail.com>
2025-06-03 16:14:04 -0400

    Allocating and destroying allocated memory.

--- memory/allocPrint.zig
+++ memory/allocPrint.zig
@@ -8,4 +8,13 @@ pub fn main() !void {
     const output = try std.fmt.allocPrint(allocator, "Hello {s}!!!", .{name});
 
     std.debug.print("{s}\n", .{output});
+
+    const some_number = try allocator.create(u32);
+    defer allocator.destroy(some_number);
+
+    some_number.* = @as(u32, 45);
+
+    std.debug.print("{d}\n", .{some_number.*});
+    // To the get the memory address, you just call the variable
+    std.debug.print("{d}\n", .{some_number});
 }
