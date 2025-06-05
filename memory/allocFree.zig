2d0348d7bf17321068b266d98e1f3623a8e95ca1
thandoncube1 <ncubethando20@gmail.com>
2025-06-05 15:50:57 -0400

    Working with allocating and freeing memory.

---
+++ memory/allocFree.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+const stdin = std.io.getStdIn();
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var input = try allocator.alloc(u8, 50);
+    defer allocator.free(input);
+    for (0..input.len) |i| {
+        input[i] = 0; // initialize all fields to zero.
+    }
+    // read user input
+    const input_reader = stdin.reader();
+    _ = try input_reader.readUntilDelimiterOrEof(input, '\n');
+    std.debug.print("{s}\n", .{input});
+}
