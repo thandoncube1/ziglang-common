fbd8ddb60f89bfae27393040daddf212453c02a1
thandoncube1 <ncubethando20@gmail.com>
2025-06-06 00:02:57 -0400

    Inital commit building the Base64 table and char_at method in Struct

---
+++ encoding/base64encoder_decoder.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+const Base64 = struct {
+    _table: *const [64]u8,
+
+    pub fn init() Base64 {
+        const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lower = "abcdefghijklmnopqrstuvwxyz";
+        const numbers_symb = "0123456789+/";
+        return Base64{
+            ._table = upper ++ lower ++ numbers_symb,
+        };
+    }
+
+    pub fn _char_at(self: Base64, index: usize) u8 {
+        return self._table[index];
+    }
+};
