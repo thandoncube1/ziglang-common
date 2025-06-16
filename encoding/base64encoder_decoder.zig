27bb44c7fa8c0577b3293ae6fca4a2d68ee9e63f
thandoncube1 <ncubethando20@gmail.com>
2025-06-15 22:26:44 -0400

    Calculating the encoding length by using the input size and multiplying the groups

--- encoding/base64encoder_decoder.zig
+++ encoding/base64encoder_decoder.zig
@@ -33,3 +33,18 @@ pub fn main() !void {
     // Print a character at an index
     try std.io.getStdOut().writer().print("Character at index 28: {c}\n", .{base64._char_at(28)});
 }
+
+// If you don’t have any previous experience with base64, you might not understand the differences between “encode” and “decode”. Essentially, the terms “encode” and “decode” here have the exact same meaning as they have in the field of encryption (i.e., they mean the same thing as “encode” and “decode” in hashing algorithms, like the MD5 algorithm).
+
+// One task that we need to do is to calculate how much space we need to reserve for the output, both of the encoder and decoder. This is simple math, and can be done easily in Zig because every array has its length (its number of elements) easily accesible by consulting the .len property of the array.
+
+// For the encoder, the logic is the following: for each 3 bytes that we find in the input, 4 new bytes are created in the output. So, we take the number of bytes in the input, divide it by 3, use a ceiling function, then, we multiply the result by 4. That way, we get the total number of bytes that will be produced by the encoder in its output.
+
+// Calculate the encoding length
+fn _calc_encode_lengt(input: []const u8) !usize {
+    if (input.len < 3) {
+        return 4;
+    }
+    const n_groups: usize = try std.math.divCeil(usize, input.len, 3);
+    return n_groups * 4;
+}
