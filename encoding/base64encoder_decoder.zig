c6d1a0a88870c2d546cc9b073f1a68f6253315f2
thandoncube1 <ncubethando20@gmail.com>
2025-06-17 15:28:39 -0400

    Fixing up some logic and structure on the Encoder and Decoder Struct

--- encoding/base64encoder_decoder.zig
+++ encoding/base64encoder_decoder.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const expect = std.testing.expect;
 
-const Base64 = struct {
+pub const Base64 = struct {
     _table: *const [64]u8,
 
     pub fn init() Base64 {
@@ -28,6 +28,114 @@ const Base64 = struct {
         }
 
         return index;
+    }
+
+    // If you don’t have any previous experience with base64, you might not understand the differences between “encode” and “decode”. Essentially, the terms “encode” and “decode” here have the exact same meaning as they have in the field of encryption (i.e., they mean the same thing as “encode” and “decode” in hashing algorithms, like the MD5 algorithm).
+
+    // One task that we need to do is to calculate how much space we need to reserve for the output, both of the encoder and decoder. This is simple math, and can be done easily in Zig because every array has its length (its number of elements) easily accesible by consulting the .len property of the array.
+
+    // For the encoder, the logic is the following: for each 3 bytes that we find in the input, 4 new bytes are created in the output. So, we take the number of bytes in the input, divide it by 3, use a ceiling function, then, we multiply the result by 4. That way, we get the total number of bytes that will be produced by the encoder in its output.
+
+    // Calculate the encoding length
+    pub fn _calc_encode_length(input: []const u8) !usize {
+        if (input.len < 3) {
+            return 4;
+        }
+        const n_groups: usize = try std.math.divCeil(usize, input.len, 3);
+        return n_groups * 4;
+    }
+
+    //  for each 4 bytes in the input, 3 bytes will be produced in the output of the decoder.
+
+    // [Logic]: we take the length of the input and divide it by 4, then we apply a floor function on the result, then we multiply the result by 3, and then, we subtract from the result how much times the character = is found in the input.
+
+    pub fn _calc_decode_length(input: []const u8) !usize {
+        if (input.len < 4) {
+            return 3;
+        }
+
+        const n_groups: usize = try std.math.divFloor(usize, input.len, 4);
+        var multiple_groups: usize = n_groups * 3;
+        var i: usize = input.len - 1;
+
+        while (i > 0) : (i -= 1) {
+            if (input[i] == '=') {
+                multiple_groups -= 1;
+            } else {
+                break;
+            }
+        }
+
+        return multiple_groups;
+    }
+
+    pub fn encode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+        if (input.len == 0) {
+            return "";
+        }
+
+        const n_out = try self._calc_encode_length(input);
+        var out = try allocator.alloc(u8, n_out);
+        var buf = [3]u8{ 0, 0, 0 };
+        var count: u8 = 0;
+        var iout: u64 = 0;
+
+        for (input, 0..) |_, i| {
+            buf[count] = input[i];
+            count += 1;
+            if (count == 3) {
+                out[iout] = self._char_at(((buf[0] & 0x03) << 4) + (buf[1] >> 4));
+                out[iout + 2] = self._char_at(((buf[1] & 0x0f) << 2) + (buf[2] >> 6));
+                out[iout + 3] = self._char_at(buf[2] & 0x3f);
+                iout += 4;
+                count = 0;
+            }
+        }
+        if (count == 1) {
+            out[iout] = self._char_at(buf[0] >> 2);
+            out[iout + 1] = self._char_at((buf[0] & 0x03) << 4);
+            out[iout + 2] = '=';
+            out[iout + 3] = '=';
+        }
+
+        if (count == 2) {
+            out[iout] = self._char_at(buf[0] >> 2);
+            out[iout + 1] = self._char_at(((buf[0] & 0x03) << 4) + (buf[1] >> 4));
+            out[iout + 2] = self._char_at((buf[1] & 0x0f) << 2);
+            out[iout + 3] = '=';
+            iout += 4;
+        }
+
+        return out;
+    }
+
+    fn decode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+        if (input.len == 0) {
+            return "";
+        }
+        const n_output = try _calc_decode_length(input);
+        var output = try allocator.alloc(u8, n_output);
+        var count: u8 = 0;
+        var iout: u64 = 0;
+        var buf = [4]u8{ 0, 0, 0, 0 };
+
+        for (0..input.len) |i| {
+            buf[count] = self._char_index(input[i]);
+            count += 1;
+            if (count == 4) {
+                output[iout] = (buf[0] << 2) + (buf[1] >> 4);
+                if (buf[2] != 64) {
+                    output[iout + 1] = (buf[1] << 4) + (buf[2] >> 2);
+                }
+                if (buf[3] != 64) {
+                    output[iout + 2] = (buf[2] << 6) + buf[3];
+                }
+                iout += 3;
+                count = 0;
+            }
+        }
+
+        return output;
     }
 };
 
@@ -58,43 +166,4 @@ pub fn main() !void {
     // Working with bits
     const bits = 0b10010111;
     try std.io.getStdOut().writer().print("{d}\n", .{bits & 0b00110000});
-}
-
-// If you don’t have any previous experience with base64, you might not understand the differences between “encode” and “decode”. Essentially, the terms “encode” and “decode” here have the exact same meaning as they have in the field of encryption (i.e., they mean the same thing as “encode” and “decode” in hashing algorithms, like the MD5 algorithm).
-
-// One task that we need to do is to calculate how much space we need to reserve for the output, both of the encoder and decoder. This is simple math, and can be done easily in Zig because every array has its length (its number of elements) easily accesible by consulting the .len property of the array.
-
-// For the encoder, the logic is the following: for each 3 bytes that we find in the input, 4 new bytes are created in the output. So, we take the number of bytes in the input, divide it by 3, use a ceiling function, then, we multiply the result by 4. That way, we get the total number of bytes that will be produced by the encoder in its output.
-
-// Calculate the encoding length
-pub fn _calc_encode_length(input: []const u8) !usize {
-    if (input.len < 3) {
-        return 4;
-    }
-    const n_groups: usize = try std.math.divCeil(usize, input.len, 3);
-    return n_groups * 4;
-}
-
-//  for each 4 bytes in the input, 3 bytes will be produced in the output of the decoder.
-
-// [Logic]: we take the length of the input and divide it by 4, then we apply a floor function on the result, then we multiply the result by 3, and then, we subtract from the result how much times the character = is found in the input.
-
-pub fn _calc_decode_length(input: []const u8) !usize {
-    if (input.len < 4) {
-        return 3;
-    }
-
-    const n_groups: usize = try std.math.divFloor(usize, input.len, 4);
-    var multiple_groups: usize = n_groups * 3;
-    var i: usize = input.len - 1;
-
-    while (i > 0) : (i -= 1) {
-        if (input[i] == '=') {
-            multiple_groups -= 1;
-        } else {
-            break;
-        }
-    }
-
-    return multiple_groups;
 }
--- encoding/encode.zig
+++ encoding/encode.zig
@@ -1,45 +1,16 @@
 const std = @import("std");
-const Base64 = @import("./base64encoder_decoder.zig");
+const encoder_decoder = @import("base64encoder_decoder.zig");
 const stdout = std.io.getStdOut().writer();
-// Initialize the Base64
-const base64 = Base64.init();
 
-pub fn encode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
-    if (input.len == 0) {
-        return "";
-    }
+pub fn main() !void {
+    var memory_buffer: [1000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&memory_buffer);
+    const allocator = fba.allocator();
 
-    const n_out = try self._calc_encode_length(input);
-    var out = try allocator.alloc(u8, n_out);
-    var buf = [3]u8{ 0, 0, 0 };
-    var count: u8 = 0;
-    var iout: u64 = 0;
+    const text = "Testing some more stuff";
+    const base64 = encoder_decoder.Base64.init();
 
-    for (input, 0..) |_, i| {
-        buf[count] = input[i];
-        count += 1;
-        if (count == 3) {
-            out[iout] = base64._char_at(((buf[0] & 0x03) << 4) + (buf[1] >> 4));
-            out[iout + 2] = base64._char_at(((buf[1] & 0x0f) << 2) + (buf[2] >> 6));
-            out[iout + 3] = base64._char_at(buf[2] & 0x3f);
-            iout += 4;
-            count = 0;
-        }
-    }
-    if (count == 1) {
-        out[iout] = self._char_at(buf[0] >> 2);
-        out[iout + 1] = self._char_at((buf[0] & 0x03) << 4);
-        out[iout + 2] = '=';
-        out[iout + 3] = '=';
-    }
-
-    if (count == 2) {
-        out[iout] = self._char_at(buf[0] >> 2);
-        out[iout + 1] = self._char_at(((buf[0] & 0x03) << 4) + (buf[1] >> 4));
-        out[iout + 2] = self._char_at((buf[1] & 0x0f) << 2);
-        out[iout + 3] = '=';
-        iout += 4;
-    }
-
-    return out;
+    const encoded_text = try base64.encode(allocator, text);
+    // Encode the text and get the response
+    try stdout.print("Encoded text: {s}\n", .{encoded_text});
 }
