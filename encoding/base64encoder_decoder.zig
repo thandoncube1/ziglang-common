b557b452e856d2ec03c165483a6afba2789f23b5
thandoncube1 <ncubethando20@gmail.com>
2025-06-17 15:17:12 -0400

    Working with Base64 struct adding a new char_index function

--- encoding/base64encoder_decoder.zig
+++ encoding/base64encoder_decoder.zig
@@ -16,6 +16,19 @@ const Base64 = struct {
     pub fn _char_at(self: Base64, index: usize) u8 {
         return self._table[index];
     }
+
+    fn _char_index(self: Base64, char: u8) u8 {
+        if (char == '=')
+            return 64;
+        var index: u8 = 0;
+        for (0..63) |i| {
+            if (self._char_at(i) == char)
+                break;
+            index += 1;
+        }
+
+        return index;
+    }
 };
 
 test "is Character at index isAlphebetic" {
