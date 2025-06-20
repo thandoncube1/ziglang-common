35c5e1c7757dc93eeaaed52b80354f4326164aba
thandoncube1 <ncubethando20@gmail.com>
2025-06-20 18:18:29 -0400

    Optionals and unwrapping optionals in Zig.

---
+++ pointers_optionals/optionals.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+
+pub fn main() !void {
+    var number: ?i32 = 21;
+    number = null; // Error null cannot be assigned as type - @TypeOf(null)
+    // but adding the ? before the type clears the error.
+    number = 5;
+    const pointer: *?i32 = &number;
+    if (pointer.*) |not_null| {
+        try stdout.print("{d}\n", .{not_null});
+    }
+}
