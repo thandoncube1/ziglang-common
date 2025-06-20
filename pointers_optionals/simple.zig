d659aad36b69f891d0e3dc14c1d5f3bdb47baefb
thandoncube1 <ncubethando20@gmail.com>
2025-06-19 22:37:03 -0400

    Structs and pointers in Zig - assigning pointers to objects.

--- pointers_optionals/simple.zig
+++ pointers_optionals/simple.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn main() void {
+pub fn main() !void {
     const number: u8 = 5;
     const pointer = &number;
 
@@ -11,4 +11,23 @@ pub fn main() void {
     // We can run oprations on pointers
     const doubled = 2 * pointer.*;
     std.debug.print("{d}\n", .{doubled});
+
+    // Working with the user struct
+    const u = User.init(1, "Pedro", "email@gmail.com");
+    const user_pointer = &u;
+    try user_pointer.*.print_name();
 }
+
+const User = struct {
+    id: u64,
+    name: []const u8,
+    email: []const u8,
+
+    fn init(id: u64, name: []const u8, email: []const u8) User {
+        return User{ .id = id, .name = name, .email = email };
+    }
+
+    fn print_name(self: User) !void {
+        try std.io.getStdOut().writer().print("{s}\n", .{self.name});
+    }
+};
