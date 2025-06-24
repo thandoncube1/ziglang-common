c688b0378e8ad46ee723616af6e4b7f36fa95111
thandoncube1 <ncubethando20@gmail.com>
2025-06-24 16:05:30 -0400

    Implementing the Socket server on the main

---
+++ HTTPServer/main.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+const SocketConf = @import("socketConnection.zig");
+const stdout = std.io.getStdOut().writer();
+
+pub fn main() !void {
+    const socket = try SocketConf.Socket.init();
+    try stdout.print("Server Addr: {any}\n", .{socket._address});
+    var server = try socket._address.listen(.{});
+    const connection = try server.accept();
+    _ = connection;
+}
