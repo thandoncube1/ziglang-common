db25ea3cab5450810cd476e6e774a910586e3b96
thandoncube1 <ncubethando20@gmail.com>
2025-06-25 22:01:11 -0400

    Working on the Zig http request.

--- HTTPServer/main.zig
+++ HTTPServer/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const SocketConf = @import("socketConnection.zig");
+const Request = @import("request.zig");
 const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
@@ -7,5 +8,10 @@ pub fn main() !void {
     try stdout.print("Server Addr: {any}\n", .{socket._address});
     var server = try socket._address.listen(.{});
     const connection = try server.accept();
-    _ = connection;
+    var buffer: [1000]u8 = undefined;
+    for (0..buffer.len) |i| {
+        buffer[i] = 0; // Initialize all values to 0 in memory.
+    }
+    _ = try Request.read_request(connection, buffer[0..buffer.len]);
+    try stdout.print("{s}\n", .{buffer});
 }
