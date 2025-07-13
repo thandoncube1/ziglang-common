6498584955b398c0d2076cb417a80f55584a6703
thandoncube1 <ncubethando20@gmail.com>
2025-07-12 23:55:59 -0400

    Test memory using Zig - Leaks return without freeing

--- unit-tests/simple-math.zig
+++ unit-tests/simple-math.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 const expect = std.testing.expect;
 
 test "testing simple sum" {
@@ -6,3 +7,12 @@ test "testing simple sum" {
     const b: u8 = 4;
     try expect((a + b) == 6);
 }
+
+fn some_memory_leak(allocator: Allocator) !void {
+    const buffer = try allocator.alloc(u32, 10);
+    _ = buffer;
+    // Return without freeing the
+    // allocated memory
+}
+
+test "memory leak" {}
