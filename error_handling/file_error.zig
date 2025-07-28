34dacde8a10fc448fb5b809d84c930e8ca01c2b6
thandoncube1 <ncubethando20@gmail.com>
2025-07-28 00:32:15 -0400

    Working with files in Zig. Error handling - catch.

--- error_handling/file_error.zig
+++ error_handling/file_error.zig
@@ -1,6 +1,10 @@
 const std = @import("std");
 const dir = std.fs.cwd();
+const logger = std.io.getStdErr();
 
 pub fn main() !void {
-    _ = dir.openFile("doesnt_exist.txt", .{});
+    _ = dir.openFile("doesnt_exist.txt", .{}) catch |err| {
+        try logger.writer().print("{any}\n", .{err});
+        return;
+    };
 }
