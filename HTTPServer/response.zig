431a9fa13869584e9f74f51853b03b50ac5006c3
thandoncube1 <ncubethando20@gmail.com>
2025-08-09 19:12:30 -0400

    Adding the index.html and 404 page.

--- HTTPServer/response.zig
+++ HTTPServer/response.zig
@@ -1,12 +1,27 @@
 const std = @import("std");
 const Connection = std.net.Server.Connection;
 
-pub fn send_200(conn: Connection) !void {
-    const message = ("HTTP/1.1 200 OK\nContent-Length: 48" ++ "\nContent-Type: text/html\n" ++ "Connection: Closed\n\n<html><body>" ++ "<h1>Hello, World!</h1></body></html>");
+pub fn send_file(conn: Connection, file_path: []const u8, status: []const u8) !void {
+    const allocator = std.heap.page_allocator;
+    const file = try std.fs.cwd().openFile(file_path, .{});
+    defer file.close();
+
+    const file_size = try file.getEndPos();
+    const buffer = try allocator.alloc(u8, file_size);
+    defer allocator.free(buffer);
+
+    _ = try file.readAll(buffer);
+
+    const message = try std.fmt.allocPrint(allocator, "HTTP/1.1 {s}\r\nContent-Length: {d}\r\nContent-Type: text/html\r\nConnection: close\r\n\r\n{s}", .{ status, buffer.len, buffer });
+    defer allocator.free(message);
+
     _ = try conn.stream.write(message);
 }
 
+pub fn send_200(conn: Connection) !void {
+    try send_file(conn, "index.html", "200 OK");
+}
+
 pub fn send_404(conn: Connection) !void {
-    const message = ("HTTP/1.1 404 Not Found\nContent-Length: 50" ++ "\nContent-Type: text/html\n" ++ "Connection: Closed\n\n<html><body>" ++ "<h1>File not found!</h1></body></html>");
-    _ = try conn.stream.write(message);
+    try send_file(conn, "404.html", "404 Not Found");
 }
